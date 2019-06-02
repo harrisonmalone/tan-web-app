9f5ad89de55382f126267a17a0a4750f10103e6f
Harrison Malone <harrison.malone2@gmail.com>
2019-06-02 21:28:30 +1000

    changed fetch for index file

--- frontend/index.js
+++ frontend/index.js
@@ -1,24 +1,32 @@
-const container = document.querySelector(".container")
+// const container = document.querySelector(".container")
 
-const renderRow = (athlete, index) => {
-  index += 1
-  const p = `
-    <p>${index} | ${athlete.name} | ${athlete.time} | ${athlete.year} | ${athlete.gender} </p>
-  `
-  container.insertAdjacentHTML("beforeend", p)
-}
+// const renderRow = (athlete, index) => {
+//   index += 1
+//   const p = `
+//     <p>${index} | ${athlete.name} | ${athlete.time} | ${athlete.year} | ${athlete.gender} </p>
+//   `
+//   container.insertAdjacentHTML("beforeend", p)
+// }
 
-const render = (data) => {
-  data.rankings.forEach((row, index) => {
-    renderRow(row, index)
-  }) 
-}
+// const render = (data) => {
+//   data.rankings.forEach((row, index) => {
+//     renderRow(row, index)
+//   }) 
+// }
 
+
+// fetch("http://localhost:5000/")
+//   .then((res) => {
+//     return res.json()
+//   })
+//   .then((data) => {
+//     render(data)
+//   })
 
 fetch("http://localhost:5000/")
   .then((res) => {
     return res.json()
   })
   .then((data) => {
-    render(data)
+    console.log(data.rankings)
   })
