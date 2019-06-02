23318527467855cb871d0a5a019e22254beb3d8d
Harrison Malone <harrison.malone2@gmail.com>
2019-06-02 21:27:53 +1000

    added cloud json file

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const express = require('express')
 const cors = require('cors')
+const axios = require('axios')
 
 // express setup
 const app = express();
@@ -10,11 +11,11 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors());
 
-app.get('/hello', (req, res) => {
-  fs.readFile('./tan_all_time.json', 'utf-8', (err, data) => {
-    if (err) throw err.message;
-    res.send(data)
-  })
+app.get('/', (req, res) => {
+  axios.get('https://thetanwebapp.s3-ap-southeast-2.amazonaws.com/tan_all_time.json')
+    .then((response) => {
+      res.send(response.data)
+    })
 })
 
 app.listen(port, () => {
