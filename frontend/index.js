6271761618805832bfc04459d37643ba1a2a89e9
Harrison Malone <harrison.malone2@gmail.com>
2019-06-02 21:38:54 +1000

    loading screen added

--- frontend/index.js
+++ frontend/index.js
@@ -1,32 +1,29 @@
-// const container = document.querySelector(".container")
+const container = document.querySelector(".container")
+const loading = document.querySelector(".loading")
 
-// const renderRow = (athlete, index) => {
-//   index += 1
-//   const p = `
-//     <p>${index} | ${athlete.name} | ${athlete.time} | ${athlete.year} | ${athlete.gender} </p>
-//   `
-//   container.insertAdjacentHTML("beforeend", p)
-// }
+const renderRow = (athlete, index) => {
+  index += 1
+  const p = `
+    <p>${index} | ${athlete.name} | ${athlete.time} | ${athlete.year} | ${athlete.gender} </p>
+  `
+  container.insertAdjacentHTML("beforeend", p)
+}
 
-// const render = (data) => {
-//   data.rankings.forEach((row, index) => {
-//     renderRow(row, index)
-//   }) 
-// }
+const render = (data) => {
+  data.rankings.forEach((row, index) => {
+    renderRow(row, index)
+  })
+  const h1 = `
+    <h1>Best Tan Times 🏃‍♂️🏃‍♀️</h1>
+  `
+  document.body.removeChild(loading)
+  container.insertAdjacentHTML("afterbegin", h1)
+}
 
-
-// fetch("http://localhost:5000/")
-//   .then((res) => {
-//     return res.json()
-//   })
-//   .then((data) => {
-//     render(data)
-//   })
-
-fetch("http://localhost:5000/")
+fetch("https://tan-web-app.harrisonnmalone.now.sh/")
   .then((res) => {
     return res.json()
   })
   .then((data) => {
-    console.log(data.rankings)
+    render(data)
   })
